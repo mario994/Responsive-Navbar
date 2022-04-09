3c9d0fdadcc7d1b90546972e068547951f1b2091
Mario <marandres994@gmail.com>
2022-04-09 09:34:02 -0500

    navbar menu completed

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import css from './css/main.css';
+const btnMenu = document.querySelector('.hamburguer-menu');
+const menuBar = document.querySelector('.menu');
+
+//Add an id to the html element
+btnMenu.addEventListener('click', () => {
+
+    if(!menuBar.getAttribute('id')){
+        //if the id doesn´t exist, add it
+        menuBar.setAttribute('id', 'show-menu');
+    } else {
+        //otherwise if the id exist, delete it
+        menuBar.setAttribute('id', '');
+    }
+
+});
